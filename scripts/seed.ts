bdecc8745e1bed011a1fcb3390c3585c9dee74a9
Vaggelis kavouras <vaggeliskavouras6@gmail.com>
2025-09-18 22:26:55 +0300

    fix(css): use :root instead of ::root so CSS variables apply

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -8,9 +8,7 @@ type RegisterSession = { id: string };
 dotenv.config({ path: '.env.local' });
 
 const supabaseUrl =
-  process.env['SUPABASE_URL'] ||
-  process.env['PUBLIC_SUPABASE_URL'] ||
-  process.env['NEXT_PUBLIC_SUPABASE_URL'];
+  process.env['PUBLIC_SUPABASE_URL'];
 const supabaseServiceKey = process.env['SUPABASE_SERVICE_ROLE_KEY'];
 
 if (!(supabaseUrl && supabaseServiceKey)) {
